84d6fbc584ce91f65908837c9c0c97fd77151710
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-01-26 13:22:42 -0300

    agregando constantes y estados a App.js

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Platform, Text, View, SafeAreaView } from "react-native";
+import { useState } from "react";
+
+const colors = ["#F7DC6F", "#A2D9CE", "#D7BDE2"];
 
 export default function App() {
+  const [isWorking, setIsWorking] = useState(false);
+  const [time, setTime] = useState(25 * 60);
+  const [currentTime, setCurrentTime] = useState("POMO" | "SHORT" | "BREAK");
+
   return (
     <SafeAreaView style={styles.container}>
       <View style={{ paddingTop: Platform.OS === "android" && 30 }}>
         <Text style={styles.text}>Pomodoro</Text>
-        <StatusBar style="auto" />
+        <Text style={styles.text}>{time}</Text>
       </View>
     </SafeAreaView>
   );
