d1eba458695f842c5f7d5f311b62e0c0d86a8f26
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-01-26 13:05:35 -0300

    primer commit

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Pomodoro</Text>
+      <Button title="My Button" onPress={() => alert("¡Hola!")} />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +14,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 32,
+    fontWeight: "bold",
+    margin: 10,
   },
 });
