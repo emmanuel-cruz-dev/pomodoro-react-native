81b246ded1c991f047f92d5d46a7393778200f9b
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-01-26 13:12:35 -0300

    agregando Platform para padding top en dispositivos android

--- App.js
+++ App.js
@@ -1,26 +1,23 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Platform, Text, View, SafeAreaView } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Pomodoro</Text>
-      <Button title="My Button" onPress={() => alert("¡Hola!")} />
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={{ paddingTop: Platform.OS === "android" && 30 }}>
+        <Text style={styles.text}>Pomodoro</Text>
+        <StatusBar style="auto" />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
   },
   text: {
     fontSize: 32,
     fontWeight: "bold",
-    margin: 10,
   },
 });
